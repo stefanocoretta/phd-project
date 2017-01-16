f9b910b6e6ee197aaa56967f114131b557454cad
Stefano Coretta <stefanoc.social@me.com>
2017-01-16 11:25:49 +0000

    change formatting

--- project-backup.bash
+++ project-backup.bash
@@ -3,36 +3,46 @@
 # Share
 
 ## Push to share
-rsync -anv --no-whole-file --exclude '*.DS_Store' ~/Documents/GitHub/phd-project/ \
+rsync -anv --no-whole-file --exclude '*.DS_Store' \
+    ~/Documents/GitHub/phd-project/ \
     /Volumes/Phonology/Common/
-rsync -avz --no-whole-file --exclude '*.DS_Store' ~/Documents/GitHub/phd-project/ \
+rsync -avz --no-whole-file --exclude '*.DS_Store' \
+    ~/Documents/GitHub/phd-project/ \
     /Volumes/Phonology/Common/
 
 ## Pull from share
-rsync -anv --no-whole-file --exclude '*.DS_Store' /Volumes/Phonology/Common/ \
+rsync -anv --no-whole-file --exclude '*.DS_Store' \
+    /Volumes/Phonology/Common/ \
     ~/Documents/GitHub/phd-project/
-rsync -avz --no-whole-file --exclude '*.DS_Store' /Volumes/Phonology/Common/ \
+rsync -avz --no-whole-file --exclude '*.DS_Store' \
+    /Volumes/Phonology/Common/ \
     ~/Documents/GitHub/phd-project/
 
 
 # Backup
 
 ## Push to external HD
-rsync -anv --no-whole-file --exclude '*.DS_Store' ~/Documents/GitHub/phd-project/ \
+rsync -anv --no-whole-file --exclude '*.DS_Store' \
+    ~/Documents/GitHub/phd-project/ \
     /Volumes/Multimedia/phd-project/
-rsync -avz --no-whole-file --exclude '*.DS_Store' ~/Documents/GitHub/phd-project/ \
+rsync -avz --no-whole-file --exclude '*.DS_Store' \
+    ~/Documents/GitHub/phd-project/ \
     /Volumes/Multimedia/phd-project/
 
 ## Pull data folder from external HD
 
 ### Pilot
-rsync -anv --no-whole-file --exclude '*.DS_Store' /Volumes/Multimedia/phd-project/pilot/data/ \
+rsync -anv --no-whole-file --exclude '*.DS_Store' \
+    /Volumes/Multimedia/phd-project/pilot/data/ \
     ~/Documents/GitHub/phd-project/pilot/data
-rsync -avz --no-whole-file --exclude '*.DS_Store' /Volumes/Multimedia/phd-project/pilot/data/ \
+rsync -avz --no-whole-file --exclude '*.DS_Store' \
+    /Volumes/Multimedia/phd-project/pilot/data/ \
     ~/Documents/GitHub/phd-project/pilot/data/
 
 ### Tracegram
-rsync -anv --no-whole-file --exclude '*.DS_Store' /Volumes/Multimedia/phd-project/pilot/data/ \
+rsync -anv --no-whole-file --exclude '*.DS_Store' \
+    /Volumes/Multimedia/phd-project/pilot/data/ \
     ~/Documents/GitHub/phd-project/tracegram/data
-rsync -avz --no-whole-file --exclude '*.DS_Store' /Volumes/Multimedia/phd-project/pilot/data/ \
+rsync -avz --no-whole-file --exclude '*.DS_Store' \
+    /Volumes/Multimedia/phd-project/pilot/data/ \
     ~/Documents/GitHub/phd-project/tracegram/data/
