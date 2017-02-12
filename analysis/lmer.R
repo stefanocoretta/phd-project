06a7d14171a2e9023347f0d72867a9bdc2ddab1f
Stefano Coretta <stefanoc.social@me.com>
2017-02-12 11:13:46 +0000

    update for polish

--- analysis/lmer.R
+++ analysis/lmer.R
@@ -4,10 +4,12 @@ library(effects)
 library(ggplot2)
 
 results_raw <- read.csv("./pilot/results/SC01_vowel_duration.csv")
-words <- read.csv("./pilot/stimuli/nonce.csv")
-results <- merge(results_raw, words, by.x = "word", by.y = "word")
+words <- read.csv("./pilot/stimuli/stimuli.csv")
+results <- merge(vowels, words, by.x = "word", by.y = "word")
 
 model <- lmer(duration ~ c2phonation * vowel + c1phonation + c2place + (1|word), data = results)
+
+model <- lmer(duration ~ c2phonation * vowel + c2place + (1|word), data = results)
 
 plot(allEffects(model))
 summary(model)
