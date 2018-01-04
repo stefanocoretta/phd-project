63fb4c927fe64ad8440f19f176a77b85ac75c957
Stefano Coretta <stefanoc.social@me.com>
2018-01-04 10:07:57 +0100

    fixes and additions

--- voicing-effect/analysis/pilot/polar.R
+++ voicing-effect/analysis/pilot/polar.R
@@ -1,11 +1,10 @@
 splines <- filter(
     splines.it,
     speaker == "it01",
-    c2place == "coronal",
     vowel == "a",
+    label == "max_TT",
     !is.na(Y)
 ) %>%
-    mutate(Y = -Y) %>%
     as.data.frame(.)
 
 ss <- polar.ssanova(
@@ -16,10 +15,22 @@ ss <- polar.ssanova(
     flip = T
 )
 
-splines_pol <- make.polar(splines, c(0.521551150793651, 51.108727))
+splines_pol <- make.polar(splines, c(-0.800933928571429, -43.434646))
 
 ggplot(splines_pol, aes(X, Y, colour = c2phonation)) +
     geom_point()
+
+splines_pol_2 <- transform_coord(splines, origin = c(-0.800933928571429, -43.434646))
+
+ggplot(splines_pol_2, aes(theta, radius, colour = c2phonation)) +
+    geom_point(alpha = 0.2) +
+    geom_smooth()
+
+splines_pol_3 <- transform_coord(splines)
+
+ggplot(splines_pol_3, aes(theta, radius, colour = c2phonation)) +
+    geom_point(alpha = 0.2) +
+    geom_smooth()
 
 splines_cart <- make.cartesian(select(splines_pol, X:Y))
 
@@ -53,3 +64,20 @@ group_by_at(df, vars(one_of(c("g1", "g2"))))
 my_enquo <- function(my_list) {
     sapply(my_list, enquo)
 }
+
+splines_o <- filter(
+    splines.it,
+    speaker == "it01",
+    c2place == "velar",
+    vowel == "o",
+    !is.na(Y)
+) %>%
+    as.data.frame(.)
+
+ss_o <- polar.ssanova(
+    splines_o,
+    "c2phonation",
+    CI.fill = TRUE,
+    is.polar = FALSE,
+    flip = T
+)
