e022720b0674a577b2ea9a246cd4eb307e1b21a2
Stefano Coretta <stefanoc.social@me.com>
2018-10-12 13:53:50 -0400

    Update italian-sz/code/save_tongue.m

--- italian-sz/code/save_tongue.m
+++ italian-sz/code/save_tongue.m
@@ -1,8 +1,12 @@
 set(gcf,'Toolbar','figure');
+
 % Then select the surface
 t = gco;
-Z = t.ZData;
+
 X = t.XData;
 Y = t.YData;
+Z = t.ZData;
+
 data = [X(:), Y(:), Z(:)];
 data = data(find(~isnan(data(:,3))),:);
+csvwrite('s5.csv', data)
