88464771eb08a782f9216a4a85d32a381d739f67
Stefano Coretta <stefanoc.social@me.com>
2018-10-23 15:07:16 -0400

    add more snippets

--- italian-sz/code/save_tongue.m
+++ italian-sz/code/save_tongue.m
@@ -1,12 +1,22 @@
-set(gcf,'Toolbar','figure');
+% set(gcf,'Toolbar','figure');
 
 % Then select the surface
-t = gco;
+% t = gco;
 
-X = t.XData;
-Y = t.YData;
-Z = t.ZData;
+% X = t.XData;
+% Y = t.YData;
+% Z = t.ZData;
+
+X = UserData.trace{1}.surface{10}.x;
+Y = UserData.trace{1}.surface{10}.y;
+Z = UserData.trace{1}.surface{10}.z;
 
 data = [X(:), Y(:), Z(:)];
 data = data(find(~isnan(data(:,3))),:);
-csvwrite('s5.csv', data)
+csvwrite('20181011121440_10.csv', data)
+
+%%%%% Read traces from cl
+
+x = traces{1}.surface{10}.x;
+y = traces{1}.surface{10}.y;
+z = traces{1}.surface{10}.z;
