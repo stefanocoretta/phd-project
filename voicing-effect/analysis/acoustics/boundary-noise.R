b69f68e6e07b6c32b9f93c4f1216038004d79b95
Stefano Coretta <stefanoc.social@me.com>
2018-11-13 18:21:24 +0100

    add boundary-noise.R

---
+++ voicing-effect/analysis/acoustics/boundary-noise.R
@@ -0,0 +1,35 @@
+v <- rnorm(500, 100, 30)
+c <- rnorm(500, 70, 20)
+noise <- rnorm(500, 10, 2)
+
+sim_dur <- tibble(
+  noise,
+  v = sample(v) - noise,
+  c = sample(c) + noise
+)
+
+v_c <- lm(
+  v ~
+    c,
+  data = sim_dur
+)
+
+summary(v_c)
+
+v <- rnorm(500, 100, 30)
+c <- v * rnorm(500, 0.7, 0.1)
+noise <- rnorm(500, 10, 2)
+
+sim_dur <- tibble(
+  noise,
+  v = v - noise,
+  c = c + noise
+)
+
+v_c <- lm(
+  v ~
+    c,
+  data = sim_dur
+)
+
+summary(v_c)
